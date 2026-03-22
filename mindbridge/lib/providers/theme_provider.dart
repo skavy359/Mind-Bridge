130087656c3caf4eff8021ab87b56548ecbb4e17
Kavy Sharma <skavy359@gmail.com>
2026-03-22 13:14:02 +0530

    Update theme_provider.dart for state management

--- mindbridge/lib/providers/theme_provider.dart
+++ mindbridge/lib/providers/theme_provider.dart
@@ -2,18 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool _isDarkMode;
+  ThemeMode _themeMode = ThemeMode.system;
 
-  ThemeProvider(this._isDarkMode);
+  ThemeProvider(bool? isDarkMode) {
+    if (isDarkMode == null) {
+      _themeMode = ThemeMode.system;
+    } else {
+      _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    }
+  }
 
-  bool get isDarkMode => _isDarkMode;
+  ThemeMode get themeMode => _themeMode;
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode = mode;
+    final prefs = await SharedPreferences.getInstance();
+    
+    if (mode == ThemeMode.system) {
+      await prefs.remove('isDarkMode');
+    } else {
+      await prefs.setBool('isDarkMode', mode == ThemeMode.dark);
+    }
+    
+    notifyListeners();
+  }
 
   Future<void> toggleTheme() async {
-    _isDarkMode = !_isDarkMode;
-
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isDarkMode', _isDarkMode);
-
-    notifyListeners();
+    if (_themeMode == ThemeMode.light) {
+      await setThemeMode(ThemeMode.dark);
+    } else {
+      await setThemeMode(ThemeMode.light);
+    }
   }
 }
